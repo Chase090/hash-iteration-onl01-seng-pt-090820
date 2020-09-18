13535f65b03074fb443aacb121aa3f8251de0197
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-18 02:13:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,12 @@ def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+=======
+ def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 end
 
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -4,14 +4,27 @@
 # suite_c: "Alfred Tennyson", 
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
+<<<<<<< HEAD
 # }
 
 def select_winner(passengers)
 winner = ""
 passengers.each do |suite, name|
+=======
+ }
+
+def select_winner(passengers)
+  winner = ""
+  passengers.each do |suite, name|
+>>>>>>> 4e0134aa66f54bb573eda94d2b450609347b544c
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
 end
+<<<<<<< HEAD
 winner
 end
+=======
+
+winner
+>>>>>>> 4e0134aa66f54bb573eda94d2b450609347b544c
